01a7d2d54a1b605e969f29c0f4ca8eb5bf674006
Atai Ambus <45061342+TheAllSeeing@users.noreply.github.com>
2018-11-26 10:03:55 +0200

    Card Class, Change Turn.

---
+++ WindowsFormsApp1/TicTacToe/card.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    class card
+    {
+        private Button CardButton;
+        private int x;
+        private int y;
+        public string content;
+        public card(int x, int y, Button button)
+        {
+            this.x = x;
+            this.y = y;
+            CardButton = button;
+            content = null;
+            button.Text = "";
+        }
+
+    }
+}
